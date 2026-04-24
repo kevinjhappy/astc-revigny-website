9da291b65b6dfcbff96dcf64ad856dacf5c33d7b
Kevin Nadin <knadin@darkmira.com>
2026-04-24 22:05:46 +0200

    ux pro max magic haappened

--- assets/app.js
+++ assets/app.js
@@ -2,15 +2,15 @@ import './styles/app.css';
 import AOS from 'aos';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import './js/navbar.js';
 import './js/hero-parallax.js';
 import './js/cards-hover.js';
 import './js/gallery-swiper.js';
 import './js/registration-modal.js';
 import './js/contact-modal.js';
 import './js/gallery-lightbox.js';
-import './js/tennis-decorations.js';
 
 gsap.registerPlugin(ScrollTrigger);
 document.addEventListener('DOMContentLoaded', () => {
-  AOS.init({ duration: 700, once: true });
+  AOS.init({ duration: 650, once: true, offset: 60 });
 });
