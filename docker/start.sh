da575b8f2f3c5ea16017934ef2002d9c08a339fb
Kevin Nadin <knadin@darkmira.com>
2026-04-26 23:20:34 +0200

    admin at start

--- docker/start.sh
+++ docker/start.sh
@@ -7,5 +7,10 @@ php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migratio
 echo "==> Warming up cache..."
 php bin/console cache:warmup
 
+if [ -n "$ADMIN_EMAIL" ] && [ -n "$ADMIN_PASSWORD" ]; then
+    echo "==> Creating admin user $ADMIN_EMAIL..."
+    php bin/console app:create-admin "$ADMIN_EMAIL" "$ADMIN_PASSWORD" || true
+fi
+
 echo "==> Starting services..."
 exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
